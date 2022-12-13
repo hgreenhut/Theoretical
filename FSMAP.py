ae3bf503c8d958c0cb710af5fabef5ace50259aa
Henry Greenhut <henrygreenhut@Henrys-Laptop.local>
2022-12-13 08:44:34 -0500

    working

--- FSMAP.py
+++ FSMAP.py
@@ -49,6 +49,51 @@ def bfs(graph, visited, first, final):
 
 first = "Olshan"
 final = "B02L"
+path = []
 print(bfs(graph, visited, first, final))
-    
+
+dfs_visited = []
+
+dfs_route = []
+
+def dfs(visited, first, final, parent = 0):
+    start_index = ""
+    end_index = ""
+    if len(visited) == 0:    
+        for i in range(len(g)):
+            if g[i][1] == first:
+                start_index = g[i][0]
+                parent =  g[i][0]
+                #print("start",start_index)
+            if g[i][1] == final:
+                end_index = g[i][0]
+                #print("end",end_index)
+    else:
+        start_index = first
+        end_index = final
+    if start_index not in visited:
+        backpath[start_index] = parent
+        #backpath[]
+        if start_index == end_index:
+            #print("found it!")
+            #print(backpath)
+            dfs_route.append(g[int(start_index)][1])
+            while backpath[start_index] != start_index:
+                     
+                        dfs_route.append(g[int(backpath[start_index])][1])
+                        start_index = backpath[start_index]
+            dfs_route.reverse()
+            if dfs_route.count != 0:
+                #print("hi")
+                print(dfs_route)
+               # return dfs_route
+            else:
+                return None
+
+        #print("str" ,start_index)
+        visited.append(start_index)
+        for neighbour in g[int(start_index)][2].split(","):
+            dfs(visited, neighbour, end_index, start_index)
 #print(g)
+
+dfs(dfs_visited, first, final)
